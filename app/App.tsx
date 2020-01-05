8c8e0e8ff4456c6b53aa2cd2baa11402e272ad57
Sam Seng <sseng1@bloomberg.net>
2020-01-05 16:27:53 -0500

    Add capturing a screenshot and showing it

--- app/App.tsx
+++ app/App.tsx
@@ -7,6 +7,7 @@ const electronAbi = NodeAbi.getAbi("6.1.7", "electron");
 const nodeAbi = NodeAbi.getAbi("12.13.0", "node");
 
 type State = {
+    imageSrc:any,
     isRecording:boolean,
     messages:Array<string>,
 };
@@ -16,6 +17,7 @@ class App extends React.Component<{}, State> {
         super(props);
 
         this.state = {
+            imageSrc:"",
             isRecording: false,
             messages: [],
         }
@@ -32,6 +34,33 @@ class App extends React.Component<{}, State> {
     private _toggleRecord() {
       if (this.state.isRecording) {
         Electron.ipcRenderer.send("stopRecording");
+        Electron.desktopCapturer.getSources({ types: [ "screen" ] })
+        .then(async sources => {
+          for (const source of sources) {
+            if (source.name === "Screen 1") {
+              try {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                  audio: false,
+                  video: {
+                    // @ts-ignore
+                    mandatory: {
+                      chromeMediaSource: "desktop",
+                      chromeMediaSourceId: source.id,
+                    }
+                  }
+                });
+                console.log("CORRECT:", stream);
+                this._handleStream(stream);
+              }
+              catch (error) {
+                console.error("Error getting user media", error);
+              }
+            }
+            else {
+              console.warn("GOT SOURCE:", source.name);
+            }
+          }
+        });
       }
       else {
         Electron.ipcRenderer.send("startRecording");
@@ -49,8 +78,50 @@ class App extends React.Component<{}, State> {
               <button onClick={this._toggleRecord.bind(this)}>
                 { this.state.isRecording ? "Stop recording" : "Start Recording" }
               </button>
+              <br />
+              <img src={this.state.imageSrc}/>
             </div>
         );
+    }
+
+    private _handleStream(stream) {
+      // Create hidden video tag
+      var video = document.createElement('video');
+      video.style.cssText = 'position:absolute;top:-10000px;left:-10000px;';
+
+      // Event connected to stream
+      video.onloadedmetadata = () => {
+          // Set video ORIGINAL height (screenshot)
+          const HEIGHT = 500;
+          const WIDTH = 500;
+          const imageFormat = "image/jpeg";
+          video.style.height = HEIGHT + 'px'; // videoHeight
+          video.style.width = WIDTH + 'px'; // videoWidth
+
+          video.play();
+
+          // Create canvas
+          var canvas = document.createElement('canvas');
+          canvas.width = HEIGHT
+          canvas.height = WIDTH
+          var ctx = canvas.getContext('2d');
+          // Draw video on canvas
+          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+          this.setState({
+            imageSrc: canvas.toDataURL(imageFormat),
+          });
+
+          // Remove hidden video tag
+          video.remove();
+          try {
+              // Destroy connect to stream
+              stream.getTracks()[0].stop();
+          } catch (e) {}
+      }
+
+      video.srcObject = stream;
+      document.body.appendChild(video);
     }
 };
 
